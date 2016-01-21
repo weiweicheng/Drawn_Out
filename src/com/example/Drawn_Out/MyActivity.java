89a466d10a3c3486f947bf2f07dab6b23df6a982
Wei-Wei Cheng <chengw@ucla.edu>
2016-01-21 12:47:48 -0800

    Added Parse dependency

--- src/com/example/Drawn_Out/MyActivity.java
+++ src/com/example/Drawn_Out/MyActivity.java
@@ -2,6 +2,8 @@ package com.example.Drawn_Out;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,10 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Parse.initialize(this);
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
     }
 }
